92b3ef88467bae50f78e6a51b48e1ffe30f25a5d
Andrew <arkillam@gmail.com>
2026-03-21 16:07:54 -0400

    some fixes

--- java_lexical_website/src/main/java/lexicalwebsite/HelloController.java
+++ java_lexical_website/src/main/java/lexicalwebsite/HelloController.java
@@ -1,4 +1,4 @@
-package funwithelastic;
+package lexicalwebsite;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- java_lexical_website/src/main/java/lexicalwebsite/LexicalWebsiteApplication.java
+++ java_lexical_website/src/main/java/lexicalwebsite/LexicalWebsiteApplication.java
@@ -1,4 +1,4 @@
-package funwithelastic;
+package lexicalwebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
